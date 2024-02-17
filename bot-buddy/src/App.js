49906dcaafe9d58afc3b15b551580ad18a7ba94e
ArianitBalidemaj <aribalid@bu.edu>
2024-02-17 16:04:39 -0500

    Routed Together

--- bot-buddy/src/App.js
+++ bot-buddy/src/App.js
@@ -3,6 +3,10 @@ import { Routes , Route } from 'react-router-dom'
 import Layout from './components/Layout'
 import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
+import Login from './components/Login'
+import Settings from './components/Settings';
+import Home from './components/Home';
+import Chat from './components/Chat';
 
 
 
@@ -10,7 +14,10 @@ function App() {
   return ( 
     <>
     <Routes>
-      <Route path="/" element={<Layout />}>  // This is to be changed
+      <Route path="/" element={<Layout />}>
+        <Route path="Home" index element={<Home />} />
+        <Route path="Settings" element={<Settings />} />
+        <Route path="Chat" element={<Chat />} />
       </Route>
     </Routes>
     </>
---
+++ bot-buddy/src/components/Settings/index.js
@@ -0,0 +1,13 @@
+import './index.scss'
+import { Link, NavLink } from 'react-router-dom'
+
+
+const Settings = () => {
+    return (
+        <div>
+            <h1>Settings</h1>
+        </div>
+    );
+}
+
+export default Settings;
