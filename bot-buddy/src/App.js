1c18a9916ee5052f4ceb8068cea97cff46a66cde
Laerk Ndreko <laerkn23@bu.edu>
2024-02-18 01:23:25 -0500

    working

--- bot-buddy/src/App.js
+++ bot-buddy/src/App.js
@@ -1,5 +1,5 @@
 import './App.scss';
-import { Routes , Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
 import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
@@ -7,19 +7,19 @@ import Login from './components/Login'
 import Settings from './components/Settings';
 import Home from './components/Home';
 import Chat from './components/Chat';
-
+import './components/Home/index.scss';
 
 
 function App() {
-  return ( 
+  return (
     <>
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route path="Home" index element={<Home />} />
-        <Route path="Settings" element={<Settings />} />
-        <Route path="Chat" element={<Chat />} />
-      </Route>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="Home" index element={<Home />} />
+          <Route path="Settings" element={<Settings />} />
+          <Route path="Chat" element={<Chat />} />
+        </Route>
+      </Routes>
     </>
   );
 }
--- bot-buddy/src/components/Home/index.js
+++ bot-buddy/src/components/Home/index.js
@@ -1,12 +1,44 @@
+// Home.js
+import React from 'react';
 import './index.scss';
+import { Link } from 'react-router-dom';
 
 
 const Home = () => {
     return (
-        <div>
-            <h1>Home</h1>
+        <div className='home-box'>
+            <div className="home-content">
+                <h1 className="h1">BotBuddy</h1>
+                <About />
+                <ChatButton isAuthenticated={true} />
+            </div>
         </div>
     );
+};
+
+const About = () => {
+    return (
+        <div className="about">
+            <h2 className="h2">The Mental Health App Designed To Connect:</h2>
+            <p className="p2">Everyone deals with problems in their life, and many people unfortunately have the feeling that they don't have the opportunity to talk to others. In fact, a study done in 2019 by the New York Post stated that "1 in 4 Americans feel like they have no one to confide in." This website was created in order to help people dealing with mental health struggles connect with other people. We want BotBuddy users to create meaningful connections safely and securely by connecting anonymously.</p>
+        </div>
+    )
 }
+
+const ChatButton = ({ isAuthenticated }) => {
+    const handleClick = () => {
+        if (isAuthenticated) {
+            window.location.href = '/chat';
+        } else {
+            window.location.href = '/login';
+        }
+    };
+
+    return (
+        <button className="chat-button" onClick={handleClick}>
+            Chat Now!
+        </button>
+    );
+};
 
 export default Home;
