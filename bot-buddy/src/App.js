36956ac08aa4231c637885a1228551c0295ac8c6
Laerk Ndreko <laerkn23@bu.edu>
2024-02-18 03:30:48 -0500

    working

--- bot-buddy/src/App.js
+++ bot-buddy/src/App.js
@@ -1,40 +1,10 @@
-<<<<<<< Updated upstream
-import './App.scss';
-import { Routes, Route } from 'react-router-dom'
-import Layout from './components/Layout'
-import { useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
-import Login from './components/Login'
-import Settings from './components/Settings';
-=======
 import React from 'react';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
->>>>>>> Stashed changes
 import Home from './components/Home';
 import Settings from './components/Settings';
 import Chat from './components/Chat';
-<<<<<<< Updated upstream
-import SignUp from './components/SignUp';
-import './components/Home/index.scss';
-import firebase from 'firebase/app';
-import 'firebase/auth';
-
-function App() {
-  return (
-    <>
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route path="Home" index element={<Home />} />
-        <Route path="Settings" element={<Settings />} />
-        <Route path="Chat" element={<Chat />} />
-      </Route>
-      <Route path="Login" element={<Login />} />
-      <Route path="SignUp" element={<SignUp />} />
-    </Routes>
-    </>
-=======
 
 function App() {
   return (
@@ -47,7 +17,6 @@ function App() {
         </Route>
       </Routes>
     </div>
->>>>>>> Stashed changes
   );
 }
 
